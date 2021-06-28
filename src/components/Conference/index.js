64f05883f21c85c8003cc369caa5f373f3be76e0
brajendra kumar <brajendrak0068@gmail.com>
2021-06-28 18:15:59 +0530

    virtual backgruns

--- src/components/Conference/index.js
+++ src/components/Conference/index.js
@@ -9,25 +9,67 @@ const Conference = props => {
     const [localTracks, setLocalTracks] = useState([]);
     const [remoteTracks, setRemoteTracks] = useState([]);
 
+
+    const imageBackground = async()=>{
+        const videoTrack = localTracks.find(track=>track.getType()==="video");
+        const options  = {
+            backgroundEffectEnabled: true,
+            backgroundType: "image",
+            virtualSource: "https://image.shutterstock.com/z/stock-photo-jasna-lake-with-beautiful-reflections-of-the-mountains-triglav-national-park-slovenia-1707906793.jpg"
+        };
+        const effect = await SariskaMediaTransport.effects.createVirtualBackgroundEffect(options);
+        await videoTrack.setEffect(effect);
+    }
+
+    const blurBackground = async ()=>{
+        const videoTrack = localTracks.find(track=>track.getType()==="video");
+        const options  = {
+            backgroundEffectEnabled: true, 
+            backgroundType: "blur" ,
+            blurValue: 25
+        }
+        
+        const effect = await SariskaMediaTransport.effects.createVirtualBackgroundEffect(options);
+        await videoTrack.setEffect(effect);
+    }
+
+    const screenSharingBackground = async ()=>{
+        const videoTrack = localTracks.find(track=>track.getType()==="video");
+        const [ desktopTrack ] = await SariskaMediaTransport.createLocalTracks({devices: ["desktop"]});
+
+        const options = {
+            backgroundEffectEnabled: true,
+            backgroundType: "desktop-share",
+            virtualSource: desktopTrack
+        }
+
+        const effect = await SariskaMediaTransport.effects.createVirtualBackgroundEffect(options);
+        await videoTrack.setEffect(effect);
+    }
+
+    const removeBackground = async ()=>{
+        const videoTrack = localTracks.find(track=>track.getType()==="video");
+        await videoTrack.setEffect(undefined);
+    }
+
     useEffect(() => {
         const {connection} = props;
         if (!connection) {
             return;
         }
-        const room = connection.initJitsiConference({ enableVirtualBackground: true });
+        const conference = connection.initJitsiConference();
 
         const captureLocalStream = async () => {
             const tracks = await SariskaMediaTransport.createLocalTracks({devices: ["audio", "video"]});
-            tracks.forEach(async track => await room.addTrack(track));
+            tracks.forEach(async track => await conference.addTrack(track));
             setLocalTracks(tracks);
-            room.join();
+            conference.join();
         }
 
         captureLocalStream();
 
         const onConferenceJoined = async () => {
             console.log("confernce joined")
-            window.room = room;
         }
 
         const onTrackRemoved = (track) => {
@@ -42,20 +84,21 @@ const Conference = props => {
             setRemoteTracks(tracks => [...tracks, track]);
         }
 
-        room.addEventListener(SariskaMediaTransport.events.conference.CONFERENCE_JOINED, onConferenceJoined);
-        room.addEventListener(SariskaMediaTransport.events.conference.TRACK_ADDED, onRemoteTrack);
-        room.addEventListener(SariskaMediaTransport.events.conference.TRACK_REMOVED, onTrackRemoved);
+        conference.addEventListener(SariskaMediaTransport.events.conference.CONFERENCE_JOINED, onConferenceJoined);
+        conference.addEventListener(SariskaMediaTransport.events.conference.TRACK_ADDED, onRemoteTrack);
+        conference.addEventListener(SariskaMediaTransport.events.conference.TRACK_REMOVED, onTrackRemoved);
 
         return () => {
-            room.leave();
+            conference.leave();
         }
     }, [connection]);
 
     return (
         <div>
-            <button>blur background </button>
-            <button>image background</button>
-            <button>screen sharing background</button>
+            <button onClick={blurBackground}>blur background </button>
+            <button onClick={imageBackground}>image background</button>
+            <button onClick={screenSharingBackground}>screen sharing background</button>
+            <button onClick={removeBackground}>remove background</button>
             <LocalStream tracks={localTracks}/>
             <RemoteStream tracks={remoteTracks}/>
         </div>
